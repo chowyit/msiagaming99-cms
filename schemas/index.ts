ec5dbe1aa856adad58bc52d2ec220f3ceb044a01
Chow Yit <chowzz0511@gmail.com>
2023-01-05 18:32:14 +0800

    Adds schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,17 +1,17 @@
 import categoryBanner from './categoryBanner'
+import categoryBannerType from './categoryBannerType'
 import gameCategory from './gameCategory'
 import homepage from './homepage'
-import categoryBannerType from './categoryBannerType'
 import product from './product'
 import topBanner from './topBanner'
 import videoBanner from './videoBanner'
 
 export const schemaTypes = [
   gameCategory,
-  product,
-  categoryBannerType,
   categoryBanner,
-  videoBanner,
-  topBanner,
+  categoryBannerType,
   homepage,
+  product,
+  topBanner,
+  videoBanner,
 ]
